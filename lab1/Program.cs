8bb8a8a15f704c4e2eede0f5a1e3515c49eef042
Pro100sasha_HD <sasha_ablin@rambler.ru>
2023-12-24 10:55:47 +0500

    commit

---
+++ lab1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+namespace lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine().Replace(" ", "");
+            string num_buffer = "";
+            List<int> nums = new List<int>();
+            List<char> ops = new List<char>();
+            int i = 0;
+            for (; i < str.Length; i++)
+            {
+                bool parsable = int.TryParse(Char.ToString(str[i]), out _);
+                if (parsable == false)
+                {
+                    ops.Add(str[i]);
+                    nums.Add(int.Parse(num_buffer));
+                    num_buffer = "";
+                }
+                else
+                {
+                    num_buffer += Char.ToString(str[i]);
+                }
+            }
+            if (!string.IsNullOrEmpty(num_buffer))
+            {
+                nums.Add(int.Parse(num_buffer));
+            }
+            foreach (char op in ops)
+            {
+                Console.Write(op.ToString() + " ");
+            }
+            Console.WriteLine();
+            foreach (int num in nums)
+            {
+                Console.Write(num.ToString() + " ");
+            }
+        }
+    }
+}
